4cf57a827f4823d1e64ce5df45de6a9f3e260ac2
Scriptor007 <hofians.d02@htl-ottakring.ac.at>
2019-04-05 10:17:52 +0200

    Startseite mit Button+Verlinkung erstellt

--- PacManGame/PacManGame/Game.xaml.cs
+++ PacManGame/PacManGame/Game.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PacManGame
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaktionslogik für Game.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Game : Window
     {
-        public MainWindow()
+        public Game()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Game Spieleseite = new Game();
-            this.Hide();
-            Spieleseite.Show();
         }
     }
 }
